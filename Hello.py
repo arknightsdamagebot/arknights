02047a2d6db2a4b22dd65769bdf60df464da3a5d
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-02 17:11:03 +0900

    test

--- Hello.py
+++ Hello.py
@@ -1,1 +1,11 @@
+import streamlit as st
+
+def buturi(param1, param2):
+    result = param1 - param2
+    return result
+def zyutu(param3, param4):
+    result = param3 * (100 - param4) / 100
+    return result
+# ページのタイトルを設定
+st.title("アークナイツダメージシミュレータ")
 

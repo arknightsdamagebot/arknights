e3a428525e7703e5c7d89fa1c342c543905a9f4c
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-02 18:55:56 +0900

    inesu

--- Hello.py
+++ Hello.py
@@ -1,12 +1,5 @@
 import streamlit as st
 
-def buturi(param1, param2):
-    result = param1 - param2
-    return result
-def zyutu(param3, param4):
-    result = param3 * (100 - param4) / 100
-    return result
-# ページのタイトルを設定
 name_to_info = {
     "イネス": {
         "職業":"偵察兵"

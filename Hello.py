4e8374cb488d9a9b0311ceefaee9429c05320351
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-08 12:18:57 +0000

    test

--- Hello.py
+++ Hello.py
@@ -2,8 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Excelファイルからデータを読み込む
-excel_file = 'https://oitast-my.sharepoint.com/personal/omzh220247_st_oita-ed_jp/Documents/[アークナイツ表　キャラ.xlsx]Sheet1
-.xlsx'  # Excelファイルのパス
+excel_file = 'https://oitast-my.sharepoint.com/personal/omzh220247_st_oita-ed_jp/Documents/[アークナイツ表　キャラ.xlsx]Sheet1'  # Excelファイルのパス
 df = pd.read_excel(excel_file)
 
 # Streamlitアプリケーションのタイトル

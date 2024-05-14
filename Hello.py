e1b9544e317ed41eae7d345ee9a80ec5b2b56e1f
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-14 11:55:34 +0000

    てｓｔ

--- Hello.py
+++ Hello.py
@@ -82,17 +82,17 @@ uploaded_file = st.file_uploader("Upload an Excel file", type="xlsx")
 
 name = st.text_input("名前を入力してください")
 
-if uploaded_file:
-    # Excelデータを読み込む
-    df = pd.read_excel(uploaded_file)
+
+ # Excelデータを読み込む
+df = pd.read_excel(uploaded_file)
 
 
-    # 名前に一致するデータを抽出
-    filtered_df = df[df['名前'] == name]
+# 名前に一致するデータを抽出
+filtered_df = df[df['名前'] == name]
 
-    # 必要な列のみを選択
-    columns_to_display = ['名前', '所属', '職業', '職分', 'レアリティ', '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3']
-    filtered_df = filtered_df[columns_to_display]
+# 必要な列のみを選択
+columns_to_display = ['名前', '所属', '職業', '職分', 'レアリティ', '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3']
+filtered_df = filtered_df[columns_to_display]
 
-    # 抽出したデータを表示
-    st.write(filtered_df)
+# 抽出したデータを表示
+st.write(filtered_df)

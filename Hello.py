407eddd13d51b11799baf9deb72c04492cc68e0e
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-26 05:31:08 +0000

    どうにかなれ

--- Hello.py
+++ Hello.py
@@ -4,18 +4,13 @@ import openpyxl
 
 st.title('アークナイツオペレーター検索')
 
-# エクセルデータを読み込む
-df = pd.read_excel('arknights - 完成.xlsx',sheet_name="キャラクター一覧")
+@st.cache
+def load_data():
+    return pd.read_excel('arknights - 完成.xlsx', sheet_name="キャラクター一覧")
 
-names = df['名前'].unique().tolist()
-selected_name = st.selectbox("名前を選択してください", names)
+df = load_data()
 
 name = st.text_input("名前を入力してください")
-
-filtered_df = df[df['名前'] == selected_name]
-
-columns_to_display = ['名前', '所属', '職業', '職分', 'レアリティ', '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3']
-filtered_df = filtered_df[columns_to_display]
 
 # 名前に一致するデータを抽出
 filtered_row = df[df['名前'] == name]

f83fad5409703b04c6fdaf127e23ba4cd9d65a47
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-27 03:48:06 +0000

    oisu

--- Hello.py
+++ Hello.py
@@ -8,8 +8,8 @@ st.title('アークナイツオペレーター検索')
 df = pd.read_excel('arknights - 完成.xlsx',sheet_name="キャラクター一覧")
 
 name = st.text_input("名前を入力してください")
+filtered_df = df[df['名前'] == name]
 
-filtered_df = df[df['名前'] == selected_name]
 
 columns_to_display = ['名前', '所属', '職業', '職分', 'レアリティ', '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3']
 filtered_df = filtered_df[columns_to_display]
@@ -32,8 +32,8 @@ if not text_input.strip():
 
 names = df['名前'].unique().tolist()
 selected_name = st.selectbox("名前を選択してください", names)
+filtered_df = df[df['名前'] == selected_name]
 
-filtered_df = df[df['名前'] == name]
 
 columns_to_display = ['名前', '所属', '職業', '職分', 'レアリティ', '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3']
 filtered_df = filtered_df[columns_to_display]

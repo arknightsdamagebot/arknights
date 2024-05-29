a76afb297d31910440f204f8d65adc4854502d33
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-29 06:24:53 +0000

    kaiaku

--- Hello.py
+++ Hello.py
@@ -4,17 +4,13 @@ import random
 
 st.title('アークナイツオペレーター検索')
  
-# エクセルデータを読み込む
 df = pd.read_excel('arknights - 完成.xlsx', sheet_name="キャラクター一覧")
  
-# 名前の選択肢を作成
 names = df['名前'].unique().tolist()
 selected_name = st.selectbox("名前を選択してください", names)
  
-# テキスト入力による名前検索
 name = st.text_input("名前を入力してください")
  
-# 選択された名前で DataFrame をフィルタリング
 if name:
     filtered_df = df[df['名前'] == name]
 elif selected_name:
@@ -22,18 +18,15 @@ elif selected_name:
 else:
     filtered_df = pd.DataFrame()
  
-# 表示するカラムを選択
 columns_to_display = [
     '名前', '所属', '職業', '職分', 'レアリティ',
     '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'
 ]
 filtered_df = filtered_df[columns_to_display]
  
-# DataFrame が空でない場合にのみ結果を表示
 if not filtered_df.empty:
     st.write(filtered_df)
  
-    # 各カラムの値を個別に表示
     for column in columns_to_display:
         st.write(f"{column}: {filtered_df[column].values[0]}")
 else:

1a7a9a8e2dcf3b9ed03573112db40ef84aede9d2
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-30 18:11:18 +0900

    これでいいのでは

--- Hello.py
+++ Hello.py
@@ -8,22 +8,9 @@ df = pd.read_excel('arknights - 完成 (2).xlsx', sheet_name="キャラクター
  
 names = df['名前'].unique().tolist()
 selected_name = st.selectbox("名前を選択してください", names)
- 
-if selected_name:
-    filtered_df = df[df['名前'] == selected_name]
-else:
-    filtered_df = pd.DataFrame()
- 
+filtered_df = df[df['名前'] == selected_name]
 columns_to_display = [
     '名前', '所属', '職業', '職分', 'レアリティ',
     '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'
 ]
 filtered_df = filtered_df[columns_to_display]
- 
-if not filtered_df.empty:
-    st.write(filtered_df)
- 
-    for column in columns_to_display:
-        st.write(f"{column}: {filtered_df[column].values[0]}")
-else:
-    st.write("名前に一致するデータがありません。")

ed70e1c1155545147b50bcf1fa08ed45a2f14feb
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-05-30 18:14:06 +0900

    これでいいのでは②

--- Hello.py
+++ Hello.py
@@ -9,7 +9,6 @@ df = pd.read_excel('arknights - 完成 (2).xlsx', sheet_name="キャラクター
 names = df['名前'].unique().tolist()
 selected_name = st.selectbox("名前を選択してください", names)
 filtered_df = df[df['名前'] == selected_name]
- 
 columns_to_display = [
     '名前', '所属', '職業', '職分', 'レアリティ',
     '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'

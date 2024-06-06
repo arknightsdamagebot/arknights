214f2c54baa3581c09a147fb80bb89b19f58e54d
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-06-06 18:22:29 +0900

    どうでしょう

--- Hello.py
+++ Hello.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-import random
 from PIL import Image
 
 st.title('アークナイツオペレーター検索')
@@ -10,26 +9,31 @@ df = pd.read_excel('arknights - 完成 (4).xlsx', sheet_name="キャラクター
 names = df['名前'].unique().tolist()
 syozokus = df['所属'].unique().tolist()
 syokubuns = df['職分'].unique().tolist()
+
 selected_name = st.selectbox("名前を選択してください", names)
 selected_syozoku = st.selectbox("所属を選択してください", syozokus)
 selected_syokubun = st.selectbox("職分を選択してください", syokubuns)
-filtered_df = df[df['名前'] == selected_name]
+
+filtered_df = df[(df['名前'] == selected_name) & 
+                 (df['所属'] == selected_syozoku) & 
+                 (df['職分'] == selected_syokubun)]
+
 columns_to_display = [
     '名前', '所属', '職業', '職分', 'レアリティ',
     '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'
 ]
-filtered_df = filtered_df[columns_to_display]
+
 if not filtered_df.empty:
     st.write(filtered_df)
 
     for column in columns_to_display:
         st.write(f"{column}: {filtered_df[column].values[0]}")
     
-    image_path = f"{selected_name}.png"  # 画像のファイルパス
+    selected_image_path = f"{selected_name}.png"  # 画像のファイルパス
     try:
-        image = Image.open(image_path)
-        st.image(image_path, caption=f'{selected_name}', width=350)
+        image = Image.open(selected_image_path)
+        st.image(selected_image_path, caption=f'{selected_name}', width=350)
     except FileNotFoundError:
         st.write("画像が見つかりませんでした。")
 else:
-    st.write("名前に一致するデータがありません。")
+    st.write("条件に一致するデータがありません。")

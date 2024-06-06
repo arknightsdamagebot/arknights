e7fd84aed79023163f4e003999114c9e088fe9cf
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-06-06 18:15:28 +0900

    いけない太陽

--- Hello.py
+++ Hello.py
@@ -8,7 +8,11 @@ st.title('アークナイツオペレーター検索')
 df = pd.read_excel('arknights - 完成 (4).xlsx', sheet_name="キャラクター一覧")
  
 names = df['名前'].unique().tolist()
+syozokus = df['所属'].unique().tolist()
+syokubuns = df['職分'].unique().tolist()
 selected_name = st.selectbox("名前を選択してください", names)
+selected_syozoku = st.selectbox("所属を選択してください", syozokus)
+selected_syokubun = st.selectbox("職分を選択してください", syokubuns)
 filtered_df = df[df['名前'] == selected_name]
 columns_to_display = [
     '名前', '所属', '職業', '職分', 'レアリティ',
@@ -29,3 +33,13 @@ if not filtered_df.empty:
         st.write("画像が見つかりませんでした。")
 else:
     st.write("名前に一致するデータがありません。")
+    filtered_df = df[df['所属'] == selected_syozoku]
+columns_to_display = [
+    '名前', '所属', '職業', '職分', 'レアリティ',
+    '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'
+]
+filtered_df = df[df['職分'] == selected_syokubun]
+columns_to_display = [
+    '名前', '所属', '職業', '職分', 'レアリティ',
+    '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'
+]

d3127f65f7ebb363c9c7bae848245ec8e17bd749
arknightsdamagebot <167769981+arknightsdamagebot@users.noreply.github.com>
2024-06-07 12:40:52 +0900

    これじゃだめかな？

--- Hello.py
+++ Hello.py
@@ -11,12 +11,12 @@ names = df['名前'].unique().tolist()
 
 selected_name = st.selectbox("名前を選択してください", names)
 
-filtered_df = df[(df['名前'] == selected_name)]
-
 columns_to_display = [
     '名前', '所属', '職業', '職分', 'レアリティ',
     '公開求人', '素質1', '素質2', 'スキル1', 'スキル2', 'スキル3'
 ]
+
+filtered_df = df[(df['名前'] == selected_name)]
 
 if not filtered_df.empty:
     st.write(filtered_df)
